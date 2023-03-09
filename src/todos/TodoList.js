c4ab25a6491a041d76e7e83f525eb4212f435248
chloe.guglielmi <chloe.guglielmi@henkel.com>
2023-03-09 14:34:30 +0100

    remove css file for todolist

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,15 +1,14 @@
 import React, { useEffect } from 'react'
 import NewTodoForm from './NewTodoForm';
 import TodoListItem from './TodoListItem';
-import './TodoList.css';
 import { connect } from 'react-redux';
 import { completeTodoRequest, loadTodos, removeTodoRequest } from './thunks';
 import { getCompletedTodos, getIncompleteTodos, getIsLoading } from './selectors';
 import styled from 'styled-components';
 
-const BigRedText = styled.div`
-    font-size: 48px;
-    color: #FF0000;
+const ListWrapper = styled.div`
+    max-width: 700px;
+    margin: auto;
 `;
 
 const TodoList = ({incompleteTodos = [], completedTodos = [], onRemovedPressed, onCompletePressed, isLoading, startLoadingTodos}) => {
@@ -18,8 +17,7 @@ const TodoList = ({incompleteTodos = [], completedTodos = [], onRemovedPressed, 
     }, []);
     
     const loadingMessage = <div>Loading todos...</div>
-    const content = (<div className='list-wrapper'>
-        <BigRedText>HELLOO</BigRedText>
+    const content = (<ListWrapper>
         <NewTodoForm />
         <h3>Incomplete todos:</h3>
         {incompleteTodos.map((todo) => (
@@ -39,7 +37,7 @@ const TodoList = ({incompleteTodos = [], completedTodos = [], onRemovedPressed, 
                 onRemovedPressed={onRemovedPressed}
             />
         ))}
-    </div>);
+    </ListWrapper>);
     return isLoading ? loadingMessage : content;
 }
 
