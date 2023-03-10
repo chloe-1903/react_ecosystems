88cd487cdddf387d3ecbd27a64a6a158e5b5580a
chloe.guglielmi <chloe.guglielmi@henkel.com>
2023-03-10 16:04:50 +0100

    adding create todo test

---
+++ src/tests/reducers.test.js
@@ -0,0 +1,25 @@
+import { expect } from "chai";
+import { todos } from "../todos/reducers";
+
+describe('The todos reducer', () => {
+    it('Add a new todo when CREATE_TODO action is received', () => {
+        const fakeTodo = { text: 'hello' };
+        const fakeAction = { 
+            type: 'CREATE_TODO',
+            payload: {
+                todo: fakeTodo
+            }
+        };
+
+        const originalState = {
+            isLoading: false,
+            data: []
+        }
+        const expectedState = {
+            isLoading: false,
+            data: [fakeTodo]
+        }
+        const actualState = todos(originalState, fakeAction);
+        expect(actualState).to.deep.equal(expectedState);
+    });
+})
